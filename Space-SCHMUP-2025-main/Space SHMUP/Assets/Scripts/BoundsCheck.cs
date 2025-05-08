68ac87e3e438b528aa46f92cc28c061c63f767b2
Bebeaten <ferryb23@my.sunyulster.edu>
2025-05-08 11:46:36 -0400

    Class-5-8-25

--- Space-SCHMUP-2025-main/Space SHMUP/Assets/Scripts/BoundsCheck.cs
+++ Space-SCHMUP-2025-main/Space SHMUP/Assets/Scripts/BoundsCheck.cs
@@ -7,46 +7,61 @@ using UnityEngine;
 /// Note that this ONLY works for an orthographic Main Camera at [ 0, 0, 0 ].
 /// </summary>
 public class BoundsCheck : MonoBehaviour { // a
-	[Header("Set in Inspector")]
-	public float radius = 1f;
-	public bool keepOnScreen = true;
-	
-	[Header("Set Dynamically")]
-	public bool isOnScreen = true;
-	public float camWidth;
-	public float camHeight;
+[Header("Set in Inspector")]
+public float radius = 1f;
+public bool keepOnScreen = true;
 
-	void Awake() {
-		camHeight = Camera.main.orthographicSize; // b
-		camWidth = camHeight * Camera.main.aspect; // c
-	}
+[Header("Set Dynamically")]
+public bool isOnScreen = true;
+public float camWidth;
+public float camHeight;
+[HideInInspector]
+public bool offRight, offLeft, offUp, offDown;
 
-	void LateUpdate () { // d
-		Vector3 pos = transform.position;
-		if (pos.x > camWidth - radius) {
-			pos.x = camWidth - radius;
-			isOnScreen = false;
-		}
+void Awake() {
+	camHeight = Camera.main.orthographicSize; // b
+	camWidth = camHeight * Camera.main.aspect; // c
+}
 
-		if (pos.x < -camWidth + radius) {
-			pos.x = -camWidth + radius;
-			isOnScreen = false;
-		}
-
-		if (pos.y > camHeight - radius) {
-			pos.y = camHeight - radius;
-		} 
-		if (pos.y < -camHeight + radius) {
-			pos.y = -camHeight + radius;
-			isOnScreen = false;
-		}
-	transform.position = pos;
+void LateUpdate () { // d
+	Vector3 pos = transform.position;
 	isOnScreen = true;
+	offRight = offLeft = offUp = offDown = false; // b
+	
+	if (pos.x > camWidth - radius) {
+		pos.x = camWidth - radius;
+		isOnScreen = false;
+		offRight = true;
 	}
-	// Draw the bounds in the Scene pane using OnDrawGizmos()
-	void OnDrawGizmos () { // e
-		if (!Application.isPlaying) return;
+
+	if (pos.x < -camWidth + radius) {
+		pos.x = -camWidth + radius;
+		isOnScreen = false;
+		offLeft = true;
+	}
+
+	if (pos.y > camHeight - radius) {
+		pos.y = camHeight - radius;
+		isOnScreen = false;
+		offUp = true;
+	} 
+	
+	if (pos.y < -camHeight + radius) {
+		pos.y = -camHeight + radius;
+		isOnScreen = false;
+		offDown = true;
+	}
+	isOnScreen = !(offRight || offLeft || offUp || offDown);
+	if (keepOnScreen && !isOnScreen){
+	   transform.position = pos;
+	   isOnScreen = true;
+	   offRight = offLeft = offUp = offDown = false; // e
+	}
+}
+// Draw the bounds in the Scene pane using OnDrawGizmos()
+void OnDrawGizmos () { // e
+	if (!Application.isPlaying) return;
 		Vector3 boundSize = new Vector3(camWidth* 2, camHeight* 2, 0.1f);
 		Gizmos.DrawWireCube(Vector3.zero, boundSize);
-		}
 	}
+}
--- Space-SCHMUP-2025-main/Space SHMUP/Assets/Scripts/Enemy.cs
+++ Space-SCHMUP-2025-main/Space SHMUP/Assets/Scripts/Enemy.cs
@@ -3,29 +3,53 @@ using System.Collections.Generic; // Required for Lists and Dictionaries
 using UnityEngine; // Required for Unity
 
 public class Enemy : MonoBehaviour {
-	[Header("Set in Inspector: Enemy")]
-	public float speed = 10f; // The speed in m/s
-	public float fireRate = 0.3f; // Seconds/shot (Unused)
-	public float health = 10;
-	public int score = 100; // Points earned for destroying this
+[Header("Set in Inspector: Enemy")]
+public float speed = 10f; // The speed in m/s
+public float fireRate = 0.3f; // Seconds/shot (Unused)
+public float health = 10;
+public int score = 100; // Points earned for destroying this
+
+protected BoundsCheck bndCheck;
+
+void Awake(){
+	bndCheck = GetComponent<BoundsCheck>();
+}
+
+
+// This is a Property: A method that acts like a field
+public Vector3 pos { // a
+get {
+	return( this.transform.position );
+	} 
+	set {
+		this.transform.position = value;
+	}
+}
+
+void Update() {
+	Move();
 	
-	// This is a Property: A method that acts like a field
-	public Vector3 pos { // a
-		get {
-			return( this.transform.position );
-		} 
-		set {
-			this.transform.position = value;
-		}
+	if (bndCheck != null && bndCheck.offDown){
+		//if (pos.y < bndCheck.camHeight - bndCheck.radius){
+			Destroy(gameObject);
+		//}
 	}
+}
 
-	void Update() {
-		Move();
-	}	
-
-	public virtual void Move() { // b
-		Vector3 tempPos = pos;
-		tempPos.y -= speed * Time.deltaTime;
-		pos = tempPos;
+public virtual void Move() { // b
+	Vector3 tempPos = pos;
+	tempPos.y -= speed * Time.deltaTime;
+	pos = tempPos;
 	}
+	
+void OnCollisionEnter( Collision coll){
+	GameObject otherGO = coll.gameObject;
+	if (otherGO.tag == "ProjectileHero"){
+		Destroy(otherGO);
+		Destroy(gameObject);
+	}
+	else{
+		print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+	}
+}
 }
